dbe61f73524be6a9d0ca1edd4a1cbc91524040e8
jawher <jawherfarhat1@gmail.com>
2025-11-17 19:50:09 +0100

    v1

--- ProfileApp/models.py
+++ ProfileApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    bank_card_number = models.CharField(max_length=20, blank=True, null=True)  # 🔹 correspond au form
+    photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
