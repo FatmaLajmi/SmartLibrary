e474f95cd9bec6bab17fe33e4b88896871893214
eyouta4 <160141414+eyouta4@users.noreply.github.com>
2025-11-17 22:13:16 +0100

    Mes modifs settings

--- SmartLibrary/settings.py
+++ SmartLibrary/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'AvisApp',
     'AvisAppApi',
-    'rest_framework',
+
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
