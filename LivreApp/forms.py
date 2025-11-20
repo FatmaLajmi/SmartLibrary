5e9455a0d5d01dc1547cdb86cd0c4bb150099408
Nadia Hafhouf <nadiahafhouf01@gmail.com>
2025-11-20 16:06:16 +0100

    redimention des images

--- LivreApp/forms.py
+++ LivreApp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Livre
+from PIL import Image
+import io
 
 class LivreForm(forms.ModelForm):
     image_upload = forms.ImageField(required=False, label="Image")
@@ -11,9 +13,27 @@ class LivreForm(forms.ModelForm):
     def save(self, commit=True):
         instance = super().save(commit=False)
         file = self.cleaned_data.get('image_upload')
+
         if file:
-            instance.image_blob = file.read()  # convert to BLOB
+            # Charger l'image
+            img = Image.open(file)
+
+            # Convertir si pas en RGB
+            if img.mode != "RGB":
+                img = img.convert("RGB")
+
+            # Redimensionner à 220x318
+            img = img.resize((220, 318), Image.Resampling.LANCZOS)
+
+            # Réencoder l’image en JPEG
+            buffer = io.BytesIO()
+            img.save(buffer, format="JPEG")
+
+            # Stocker dans le blob
+            instance.image_blob = buffer.getvalue()
+
         if commit:
             instance.save()
         return instance
+
 
