71a8dd68ef1a25116f12aebe2cd8b5efff831caa
eyouta4 <160141414+eyouta4@users.noreply.github.com>
2025-11-23 19:41:18 +0100

    Metiers Avancées

--- AvisApp/models.py
+++ AvisApp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from LivreApp.models import Livre
+from django.conf import settings
 
 class Avis(models.Model):
     book_id = models.ForeignKey(Livre, on_delete=models.CASCADE, related_name='avis')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='avis',null=True, blank=True)
     note = models.IntegerField()  # 0-5
     commentaire = models.TextField(blank=True)
     date = models.DateField(auto_now_add=True)
--- AvisApp/views.py
+++ AvisApp/views.py
@@ -7,6 +7,9 @@ from .forms import AvisForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import Http404
+from django.db.models import Q, Avg 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class AvisListView(ListView):
     model = Avis
@@ -16,12 +19,22 @@ class AvisListView(ListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
+
         # Filtrer par livre si book_id est passé en paramètre
         book_id = self.request.GET.get('book_id')
         if book_id:
             qs = qs.filter(book_id=book_id)
-        
-        # Tri via param ?order=date or ?order=note
+
+        # 🔍 Recherche (titre, auteur, commentaire)
+        q = self.request.GET.get('q')
+        if q:
+            qs = qs.filter(
+                Q(book_id__title__icontains=q) |
+                Q(book_id__author__icontains=q) |
+                Q(commentaire__icontains=q)
+            )
+
+        # Tri via ?order=date / -date / note / -note
         order = self.request.GET.get('order', '-date')
         if order not in ('date', '-date', 'note', '-note'):
             order = '-date'
@@ -32,18 +45,28 @@ class AvisListView(ListView):
         book_id = self.request.GET.get('book_id')
         if book_id:
             try:
-                context['livre'] = Livre.objects.get(pk=book_id)
+                livre = Livre.objects.get(pk=book_id)
+                context['livre'] = livre
+
+                # ⭐ Moyenne des notes pour ce livre
+                agg = livre.avis.aggregate(
+                    avg_note=Avg('note'),
+                )
+                context['avg_note'] = agg['avg_note']
+                context['nb_avis'] = livre.avis.count()
             except Livre.DoesNotExist:
                 pass
+
+        context['search_query'] = self.request.GET.get('q', '')
         return context
 
-class AvisCreateView(CreateView):
+class AvisCreateView(LoginRequiredMixin, CreateView):
     model = Avis
     form_class = AvisForm
     template_name = 'Avis/ajouter_avis.html'
-    
+    login_url = 'login'  # ou le nom de ta route de login
+
     def dispatch(self, request, *args, **kwargs):
-        # Vérifier que book_id est fourni
         self.book_id = kwargs.get('book_id') or request.GET.get('book_id')
         if not self.book_id:
             messages.error(request, "Aucun livre sélectionné.")
@@ -63,17 +86,24 @@ class AvisCreateView(CreateView):
     
     def form_valid(self, form):
         form.instance.book_id = self.livre
+        form.instance.user = self.request.user   # 🔴 lier au user
         messages.success(self.request, "Avis ajouté avec succès.")
         return super().form_valid(form)
     
     def get_success_url(self):
         return reverse_lazy('avis_list') + f'?book_id={self.livre.pk}'
 
-class AvisUpdateView(UpdateView):
+class AvisUpdateView(LoginRequiredMixin, UpdateView):
     model = Avis
     form_class = AvisForm
     template_name = 'Avis/modifier_avis.html'
-    
+    login_url = 'login'
+
+    def get_queryset(self):
+        # 🔒 Restreindre aux avis de l'utilisateur connecté
+        base_qs = super().get_queryset()
+        return base_qs.filter(user=self.request.user)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['livre'] = self.object.book_id
@@ -86,15 +116,35 @@ class AvisUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('avis_list') + f'?book_id={self.object.book_id.pk}'
 
-class AvisDeleteView(DeleteView):
+class AvisDeleteView(LoginRequiredMixin, DeleteView):
     model = Avis
     template_name = 'Avis/supprimer_avis.html'
-    
+    login_url = 'login'
+
+    def get_queryset(self):
+        base_qs = super().get_queryset()
+        return base_qs.filter(user=self.request.user)
+
     def get_success_url(self):
         book_id = self.object.book_id.pk
         messages.success(self.request, "Avis supprimé.")
         return reverse_lazy('avis_list') + f'?book_id={book_id}'
+    
 
+class MesAvisListView(LoginRequiredMixin, ListView):
+    model = Avis
+    template_name = 'Avis/mes_avis.html'
+    context_object_name = 'avis_list'
+    paginate_by = 8
+    login_url = 'login'
+
+    def get_queryset(self):
+        return Avis.objects.filter(user=self.request.user).select_related('book_id').order_by('-date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_avis'] = self.get_queryset().count()
+        return context
 class AvisDetailView(DetailView):
     model = Avis
     template_name = 'Avis/detail_avis.html'
